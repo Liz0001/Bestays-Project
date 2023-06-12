535542b47469137a71a60c729d8dcddaf6d7b033
Liis U <liis.usin@gmail.com>
2023-06-12 15:50:22 +0200

    validate password middleware function

---
+++ server/src/middlewares/validatePasswords.ts
@@ -0,0 +1,20 @@
+export function validatePassword(password: string) {
+    const uppercaseRegex = /[A-Z]/;
+    const lowercaseRegex = /[a-z]/;
+    const digitRegex = /[0-9]/;
+    const specialSymbolRegex = /[!@#$%^&*]/;
+
+    const hasUppercase = uppercaseRegex.test(password);
+    const hasLowercase = lowercaseRegex.test(password);
+    const hasDigit = digitRegex.test(password);
+    const hasSpecialSymbol = specialSymbolRegex.test(password);
+    const isWithinLengthRange = password.length > 5 && password.length < 50;
+
+    return (
+        hasUppercase &&
+        hasLowercase &&
+        hasDigit &&
+        hasSpecialSymbol &&
+        isWithinLengthRange
+    );
+}
