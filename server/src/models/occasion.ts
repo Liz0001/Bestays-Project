a908beecc84f99d2bbb5eda3061a1078e716a5dd
Liis U <liis.usin@gmail.com>
2023-06-14 19:35:49 +0200

    init template for occasions

---
+++ server/src/models/occasion.ts
@@ -0,0 +1,28 @@
+// https://medium.com/@nafiu.dev/building-an-advanced-backend-api-from-scratch-using-node-js-f26ed5c7092c#75be
+
+// Occasion: A specific event or happening that is noteworthy or special, often celebrated or observed.
+
+// Birthdays:
+// ----------
+// Date of birth
+// Birth anniversary
+// Natal day
+// Born day
+// Celebration of life
+// B-day
+
+// Important Days:
+// ---------------
+// Significant dates
+// Memorable occasions
+// Meaningful milestones
+// Key events
+// Commemorative days
+
+// Special Days:
+// -------------
+// Special occasions
+// Notable events
+// Red-letter days
+// Remarkable moments
+// Festive occasions
