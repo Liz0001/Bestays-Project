9cd7b9905f8e40142ce0c8138e41ee595120b550
Liis U <liis.usin@gmail.com>
2023-07-14 19:46:09 +0200

    themeProvider init

---
+++ client/src/config/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme, colors } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: colors.orange[500],
+        },
+    },
+});
+
+export default theme;
