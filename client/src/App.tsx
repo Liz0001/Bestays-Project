aa83f6883a99e049e5b66ae861cdaded601541a8
Liis U <liis.usin@gmail.com>
2023-07-14 19:50:22 +0200

    app route / themeprovider update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-// import { ThemeProvider } from '@mui/material';
-// import { theme } from './config/theme';
+import { ThemeProvider } from '@mui/material';
+import theme from './config/theme';
 
 import { Contact } from './components/contact/Contact';
 import { AboutPage } from './components/aboutPage/AboutPage';
@@ -8,14 +8,15 @@ import { LandingPage } from './components/landingPage/LandingPage';
 import { Page404 } from './components/page404/Page404';
 
 export default function App() {
-    // <ThemeProvider theme={theme}> wrap around .... </ThemeProvider>
     // <Route path="/dashboard" element={<Dasboard />} />
     return (
-        <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<Page404 />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<Page404 />} />
+            </Routes>
+        </ThemeProvider>
     );
 }
