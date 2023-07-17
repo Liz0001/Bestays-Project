af4d34ea63ba328fcd5a6d0c53b35d927bc8ac7e
Liis U <liis.usin@gmail.com>
2023-07-17 18:31:18 +0200

    simple 404

--- client/src/components/page404/Page404.tsx
+++ client/src/components/page404/Page404.tsx
@@ -1,3 +1,16 @@
+import './page404.css';
+
 export function Page404() {
-    return <div>Page404</div>;
+    return (
+        <div className="not-found">
+            <div className="text-404">
+                <h2>Oi Oops ... </h2>
+                <p>We couldn't find this page!</p>
+                <p>
+                    Seems like <span className="oops">{location.pathname}</span>{' '}
+                    is not part of this site!
+                </p>
+            </div>
+        </div>
+    );
 }
