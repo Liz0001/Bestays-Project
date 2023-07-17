6fa46167812be0850872f542772c2d2090441ee0
Liis U <liis.usin@gmail.com>
2023-07-17 18:19:13 +0200

    app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,17 +6,29 @@ import { Contact } from './components/contact/Contact';
 import { AboutPage } from './components/aboutPage/AboutPage';
 import { LandingPage } from './components/landingPage/LandingPage';
 import { Page404 } from './components/page404/Page404';
+import { Footer } from './components/footer/Footer';
+import { NavbarPublic } from './components/navbars/navbarPublic/NavbarPublic';
 
 export default function App() {
-    // <Route path="/dashboard" element={<Dasboard />} />
+    const welcomePages = ['/', '/about', '/contact'].includes(
+        window.location.pathname
+    );
+
     return (
         <ThemeProvider theme={theme}>
+            {welcomePages && <NavbarPublic />}
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/about" element={<AboutPage />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path="/bestays/">
+                    {/* <Route path="/" element={<Board />} /> */}
+                    {/* <Route path="/profile" element={<Profile />} /> */}
+                </Route>
                 <Route path="*" element={<Page404 />} />
+                {/* <Route path="/admin" element={<Admin />} /> */}
             </Routes>
+            {welcomePages && <Footer />}
         </ThemeProvider>
     );
 }
