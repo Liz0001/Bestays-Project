30de41fde94b82d1d23fbbd71bf7337cbe418528
Liis U <liis.usin@gmail.com>
2023-07-19 19:23:32 +0200

    navbar theme added

--- client/src/config/theme.ts
+++ client/src/config/theme.ts
@@ -1,9 +1,30 @@
 import { createTheme, colors } from '@mui/material';
+// #E16913 - font main orange
+// #565656 - fort secondary dark
 
 const theme = createTheme({
     palette: {
         primary: {
-            main: colors.orange[500],
+            main: '#E16913',
+            // contrastText: '#E16913',
+        },
+        secondary: {
+            main: '#565656',
+        },
+        background: {
+            default: '#fff',
+        },
+    },
+    typography: {
+        fontSize: 14,
+    },
+    components: {
+        MuiAppBar: {
+            styleOverrides: {
+                colorPrimary: {
+                    backgroundColor: '#fff',
+                },
+            },
         },
     },
 });
