6789451d04eca47fb7b966c83b59f54acee100c2
Liis U <liis.usin@gmail.com>
2023-07-19 14:13:35 +0200

    404 page updated with button and navbar

--- client/src/components/page404/Page404.tsx
+++ client/src/components/page404/Page404.tsx
@@ -1,16 +1,23 @@
+import { Link } from '@mui/material';
+import { NavbarPublic } from '../navbars/navbarPublic/NavbarPublic';
 import './page404.css';
 
 export function Page404() {
     return (
-        <div className="not-found">
-            <div className="text-404">
-                <h2>Oi Oops ... </h2>
-                <p>We couldn't find this page!</p>
-                <p>
-                    Seems like <span className="oops">{location.pathname}</span>{' '}
-                    is not part of this site!
-                </p>
+        <>
+            <div className="not-found">
+                <NavbarPublic />
+                <div className="text-404">
+                    <h2>Oi Oops ... </h2>
+                    <p>We couldn't find this page!</p>
+                    <p>
+                        Seems like{' '}
+                        <span className="oops">{location.pathname}</span> is not
+                        part of this site!
+                    </p>
+                    <Link href="/">GO BACK HOME</Link>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
