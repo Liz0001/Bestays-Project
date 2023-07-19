380e0243dd501eec24c98edf46f7cdeb736da5da
Liis U <liis.usin@gmail.com>
2023-07-19 15:38:17 +0200

    app public and private routes layed out

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
 import theme from './config/theme';
 
-import { Contact } from './components/contact/Contact';
-import { AboutPage } from './components/aboutPage/AboutPage';
 import { LandingPage } from './components/landingPage/LandingPage';
+import { AboutPage } from './components/aboutPage/AboutPage';
+import { Contact } from './components/contact/Contact';
 import { Page404 } from './components/page404/Page404';
 import { Footer } from './components/footer/Footer';
-import { NavbarPublic } from './components/navbars/navbarPublic/NavbarPublic';
 
 export default function App() {
     const welcomePages = ['/', '/about', '/contact'].includes(
@@ -16,16 +15,19 @@ export default function App() {
 
     return (
         <ThemeProvider theme={theme}>
-            {welcomePages && <NavbarPublic />}
             <Routes>
+                {/* Public routes */}
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/about" element={<AboutPage />} />
                 <Route path="/contact" element={<Contact />} />
-                <Route path="/bestays/">
-                    {/* <Route path="/" element={<Board />} /> */}
-                    {/* <Route path="/profile" element={<Profile />} /> */}
-                </Route>
-                <Route path="*" element={<Page404 />} />
+                <Route path="/*" element={<Page404 />} />
+
+                {/* PRIVATE ROUTES LATER */}
+                {/* <Route path="/bestays/*"> */}
+                {/* <Route path="" element={<Board />} /> */}
+                {/* <Route path="/profile" element={<Profile />} /> */}
+                {/* <Route path="*" element={<Page404 />} /> */}
+                {/* </Route> */}
                 {/* <Route path="/admin" element={<Admin />} /> */}
             </Routes>
             {welcomePages && <Footer />}
