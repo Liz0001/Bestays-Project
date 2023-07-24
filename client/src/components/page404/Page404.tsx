81ab5b4095a3adbf743f2f95753fa893bc17b8f5
Liis U <liis.usin@gmail.com>
2023-07-24 16:24:27 +0200

    updated 404 with navbar + text bigger with shadow

--- client/src/components/page404/Page404.tsx
+++ client/src/components/page404/Page404.tsx
@@ -5,8 +5,8 @@ import './page404.css';
 export function Page404() {
     return (
         <>
+            <NavbarPublic />
             <div className="not-found">
-                <NavbarPublic />
                 <div className="text-404">
                     <h2>Oi Oops ... </h2>
                     <p>We couldn't find this page!</p>
