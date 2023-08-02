b9c9282e2aa1899e3f4d336dbaec44a4771816d8
Liis U <liis.usin@gmail.com>
2023-08-02 20:11:19 +0200

    Size changes - everything made smalles - top section hero part

--- client/src/components/landingPage/HeroSection.tsx
+++ client/src/components/landingPage/HeroSection.tsx
@@ -1,4 +1,4 @@
-import { Button, Container } from '@mui/material';
+import { Button } from '@mui/material';
 import balloons from '../../assets/images/balloons.png';
 import './landingPage.css';
 
@@ -6,32 +6,30 @@ export function HeroSection() {
     return (
         <div className="bg-container">
             <div className="bg-image">
-                <Container>
-                    <div className="landing-page">
-                        <div className="wrapper">
-                            <div className="balloon-img">
-                                <img src={balloons} alt="Bestay balloons" />
-                            </div>
-                            <h1 className="heading">
-                                Never miss a moment worth celebrating
-                            </h1>
-                            <p className="value-proposition">
-                                Sign up for Bestays now and stay organized,
-                                cherish your best days, and create lasting
-                                memories effortlessly!
-                            </p>
-                            <div className="button-wrap">
-                                <Button
-                                    href="/signup"
-                                    variant="outlined"
-                                    size="large"
-                                >
-                                    Sign up now
-                                </Button>
-                            </div>
+                <div className="landing-page">
+                    <div className="wrapper">
+                        <div className="balloon-img">
+                            <img src={balloons} alt="Bestay balloons" />
+                        </div>
+                        <h1 className="heading">
+                            Never miss a moment worth celebrating
+                        </h1>
+                        <p className="value-proposition">
+                            Sign up for Bestays now and stay organized, cherish
+                            your best days, and create lasting memories
+                            effortlessly!
+                        </p>
+                        <div className="button-wrap">
+                            <Button
+                                href="/signup"
+                                variant="outlined"
+                                size="large"
+                            >
+                                Sign up now
+                            </Button>
                         </div>
                     </div>
-                </Container>
+                </div>
             </div>
         </div>
     );
