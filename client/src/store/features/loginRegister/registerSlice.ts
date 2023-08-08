f06eef67a7cf4124318921f7e548262441c6b26f
Liis U <liis.usin@gmail.com>
2023-08-08 18:36:03 +0200

    register slice working

--- client/src/store/features/loginRegister/registerSlice.ts
+++ client/src/store/features/loginRegister/registerSlice.ts
@@ -1,18 +1,52 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { PageStatus } from '../../../enums/pageStatus';
+import { AppDispatch } from '../../store';
+import instance from '../../../config/axios';
 
 interface RegisterState {
-    //
+    registerStatus: PageStatus;
 }
 
 const initialState: RegisterState = {
-    //
+    registerStatus: PageStatus.initial,
 };
 
 const registerSlice = createSlice({
     name: 'register',
     initialState,
-    reducers: {},
+    reducers: {
+        setRegisterStatus(state, action: PayloadAction<PageStatus>) {
+            state.registerStatus = action.payload;
+        },
+    },
 });
 
-export const {} = registerSlice.actions;
+export function registerUser(newUserData: any) {
+    return async function (dispatch: AppDispatch) {
+        dispatch(setRegisterStatus(PageStatus.loading));
+
+        try {
+            const res = await instance.post(
+                '/auth/register',
+                { newUserData },
+                { headers: { 'Content-Type': 'application/json' } }
+            );
+            // console.log('in registerUser, (res):', res);
+            if (res.status === 200) {
+                dispatch(setRegisterStatus(PageStatus.success));
+                // toast
+            } else {
+                dispatch(setRegisterStatus(PageStatus.error));
+                // toast
+            }
+        } catch (error: any) {
+            // console.log('In registerUser Error:', error.request.responseText);
+
+            dispatch(setRegisterStatus(PageStatus.error));
+            // toast
+        }
+    };
+}
+
+export const { setRegisterStatus } = registerSlice.actions;
 export default registerSlice.reducer;
