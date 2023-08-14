72b32051beee1771bd6732f65868890313260213
Liis U <liis.usin@gmail.com>
2023-08-14 14:49:08 +0200

    updated password difficulty + max lengths

--- server/src/middlewares/validatePasswords.ts
+++ server/src/middlewares/validatePasswords.ts
@@ -4,17 +4,15 @@ export function validatePassword(password: string) {
     const digitRegex = /[0-9]/;
     const specialSymbolRegex = /[!@#$%^&*]/;
 
-    const hasUppercase = uppercaseRegex.test(password);
-    const hasLowercase = lowercaseRegex.test(password);
+    // const hasUppercase = uppercaseRegex.test(password);
+    // const hasLowercase = lowercaseRegex.test(password);
     const hasDigit = digitRegex.test(password);
     const hasSpecialSymbol = specialSymbolRegex.test(password);
-    const isWithinLengthRange = password.length > 5 && password.length < 50;
+    const isWithinLengthRange = password.length >= 6;
 
     return (
-        hasUppercase &&
-        hasLowercase &&
-        hasDigit &&
-        hasSpecialSymbol &&
-        isWithinLengthRange
+        // hasUppercase &&
+        // hasLowercase &&
+        hasDigit && hasSpecialSymbol && isWithinLengthRange
     );
 }
