c61d4a08e5ec8b4df3096581018ec215cd512b18
Liis U <liis.usin@gmail.com>
2023-08-22 19:38:11 +0200

    implement registration form design

--- client/src/components/loginRegister/register/Register.tsx
+++ client/src/components/loginRegister/register/Register.tsx
@@ -2,5 +2,12 @@ import { RegForm } from './RegForm';
 import './register.css';
 
 export function Register() {
-    return <RegForm />;
+    return (
+        <>
+            <div className="register-container">
+                <div className="register-bg-image"></div>
+                <RegForm />
+            </div>
+        </>
+    );
 }
